ed149ab523ec7769006bae180e816f7c0ed2a1ac
Nayaksonal <36729338+Nayaksonal@users.noreply.github.com>
2019-11-14 23:41:15 +0000

    Add files via upload

--- tests/test_repeating_letter.py
+++ tests/test_repeating_letter.py
@@ -14,3 +14,19 @@ def test_valid_VV(capsys):
     top_level_calc(input_str)
     captured = capsys.readouterr()
     assert f'roman input VV is Invalid' in captured.out
+
+
+def test_invalid_for_continous_four_sameletter(capsys):
+    input_str = 'IIII'
+    top_level_calc(input_str)
+    captured = capsys.readouterr()
+    assert f'roman input IIII is Invalid' in captured.out
+
+
+def test_invalid_combination(capsys):
+    input_str = 'IM'
+    top_level_calc(input_str)
+    captured = capsys.readouterr()
+    print('trting to invalid combincation')
+    print(captured.out)
+    assert f'roman input IM is Invalid' in captured.out
